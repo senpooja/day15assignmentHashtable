21e1e8c2124c570ea863c045932d75679aceaa41
senpooja <poojasen744055@gmail.com>
2022-07-31 15:52:58 +0530

    wellcome  bst-uc-2 file

---
+++ bstuc-2/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("\\nHello World!"); //[3,7,9,11,55,67]
+BinarySearchTree<int> binarySearch = new BinarySearchTree<int>(56);
+binarySearch.Insert(30);
+binarySearch.Insert(70);
+binarySearch.Insert(22);
+binarySearch.Insert(40);
+binarySearch.Insert(60);
+binarySearch.Insert(95);
+binarySearch.Insert(11);
+binarySearch.Insert(65);
+binarySearch.Insert(3);
+binarySearch.Insert(16);
+binarySearch.Insert(63);
+binarySearch.Insert(67);
+binarySearch.Display();
+binarySearch.GetSize();
+
+Console.ReadKey();
