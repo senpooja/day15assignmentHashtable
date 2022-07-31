b55d3f84af544fcd431a4102329ddaf63a8b0ca4
senpooja <poojasen744055@gmail.com>
2022-07-31 15:47:48 +0530

    wellcome  hashtableuc-2 file

---
+++ hashtableuc-2/Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Hash table demo"); //() []
+MyMapNode<string, string> hash = new MyMapNode<string, string>(5);
+hash.Add("0", "Paranoid");
+hash.Add("1", "are");
+hash.Add("2", "not");
+hash.Add("3", "paranoid");
+hash.Add("4", "becouse");
+hash.Add("5", "they");
+hash.Add("6", "are");
+hash.Add("7", "putting");
+hash.Add("8", "themselves");
+hash.Add("9", "deliberately");
+hash.Add("10", "into");
+hash.Add("11", "situations");
+string hash0 = hash.Get("0");
+string hash1 = hash.Get("1");
+string hash2 = hash.Get("2");
+string hash3 = hash.Get("3");
+string hash4 = hash.Get("4");
+string hash5 = hash.Get("5");
+//string hash1 = hash.Get("6");
+//string hash2 = hash.Get("7");
+//string hash3 = hash.Get("8");
+//string hash4 = hash.Get("9");
+//string hash5 = hash.Get("10");
+//string hash5 = hash.Get("11");
+//Console.WriteLine("2nd index value: " + hash2);
+//Console.WriteLine("3rd index value: " + hash3);
+//Console.WriteLine("4th index value: " + hash4);
+//Console.WriteLine("5th index value: " + hash5);
+//Console.WriteLine("1st index value: " + hash1);
